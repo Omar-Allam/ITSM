62a6d95eb9b078160cf83b010fec6c324d2de930
Hazem Mohamed <hazem.mohamed@alkifah.com>
2016-12-22 16:31:02 +0300

    Modify add/edit SLA page

--- resources/views/admin/sla/_form.blade.php
+++ resources/views/admin/sla/_form.blade.php
@@ -19,67 +19,83 @@
 
         @include('admin.partials._criteria')
 
-        <div class="panel panel-sm panel-danger">
+        <section class="panel panel-sm panel-danger">
             <div class="panel-heading">
                 <h4 class="panel-title">Due by time</h4>
             </div>
-            <div class="panel-body row">
-                <div class="col-md-4">
-                    <div class="form-group form-group-sm {{$errors->has('due_days')? 'has-error' : ''}}">
-                        {{ Form::label('due_days', 'Days', ['class' => 'control-label']) }}
-                        {{ Form::text('due_days', null, ['class' => 'form-control']) }}
-                    </div>
+            <table class="table table-bordered table-condensed">
+                <thead>
+                    <tr>
+                        <th class="col-sm-4 bg-danger">{{ Form::label('due_days', 'Days', ['class' => 'control-label']) }}</th>
+                        <th class="col-sm-4 bg-danger">{{ Form::label('due_hours', 'Hours', ['class' => 'control-label']) }}</th>
+                        <th class="col-sm-4 bg-danger">{{ Form::label('due_minutes', 'Minutes', ['class' => 'control-label']) }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ Form::text('due_days', null, ['class' => 'form-control input-sm']) }}</td>
+                        <td>{{ Form::selectRange('due_hours', 0, 23, null, ['class' => 'form-control input-sm']) }}</td>
+                        <td>{{ Form::selectRange('due_minutes', 0, 59, null, ['class' => 'form-control input-sm']) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            @if ($errors->has('due_days'))
+                <div class="panel-footer">
+                    <div class="error-message">{{$errors->first('due_days')}}</div>
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group form-group-sm {{$errors->has('due_days')? 'has-error' : ''}}">
-                        {{ Form::label('due_hours', 'Hours', ['class' => 'control-label']) }}
-                        {{ Form::selectRange('due_hours', 0, 23, null, ['class' => 'form-control']) }}
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group form-group-sm {{$errors->has('due_days')? 'has-error' : ''}}">
-                        {{ Form::label('due_minutes', 'Minutes', ['class' => 'control-label']) }}
-                        {{ Form::selectRange('due_minutes', 0, 59, null, ['class' => 'form-control']) }}
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    @if ($errors->has('due_days'))
-                        <div class="error-message">{{$errors->first('due_days')}}</div>
-                    @endif
-                </div>
-            </div>
-        </div>
+            @endif
+        </section>
 
-        <div class="panel panel-sm panel-warning">
+        <section class="panel panel-sm panel-warning">
             <div class="panel-heading">
                 <h4 class="panel-title">First response time</h4>
             </div>
-            <div class="panel-body row">
-                <div class="col-md-4">
-                    <div class="form-group form-group-sm {{$errors->has('response_days')? 'has-error' : ''}}">
-                        {{ Form::label('response_days', 'Days', ['class' => 'control-label']) }}
-                        {{ Form::text('response_days', null, ['class' => 'form-control']) }}
-                    </div>
+            
+            <table class="table table-bordered table-condensed">
+                <thead>
+                    <tr>
+                        <th class="col-sm-4 bg-warning">{{ Form::label('response_days', 'Days', ['class' => 'control-label']) }}</th>
+                        <th class="col-sm-4 bg-warning">{{ Form::label('response_hours', 'Hours', ['class' => 'control-label']) }}</th>
+                        <th class="col-sm-4 bg-warning">{{ Form::label('response_minutes', 'Minutes', ['class' => 'control-label']) }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ Form::text('response_days', null, ['class' => 'form-control input-sm']) }}</td>
+                        <td>{{ Form::selectRange('response_hours', 0, 23, null, ['class' => 'form-control input-sm']) }}</td>
+                        <td>{{ Form::selectRange('response_minutes', 0, 59, null, ['class' => 'form-control input-sm']) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            @if ($errors->has('response_days'))
+                <div class="panel-footer">
+                    <div class="error-message">{{$errors->first('response_days')}}</div>        
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group form-group-sm {{$errors->has('response_days')? 'has-error' : ''}}">
-                        {{ Form::label('response_hours', 'Hours', ['class' => 'control-label']) }}
-                        {{ Form::selectRange('response_hours', 0, 23, null, ['class' => 'form-control']) }}
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group form-group-sm {{$errors->has('response_days')? 'has-error' : ''}}">
-                        {{ Form::label('response_minutes', 'Minutes', ['class' => 'control-label']) }}
-                        {{ Form::selectRange('response_minutes', 0, 59, null, ['class' => 'form-control']) }}
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    @if ($errors->has('response_days'))
-                        <div class="error-message">{{$errors->first('response_days')}}</div>
-                    @endif
-                </div>
+            @endif
+        </section>
+
+        <section class="panel panel-default panel-sm panel-violet">
+            <div class="panel-heading">
+                <h4 class="panel-title">Approval Escalation</h4>
             </div>
-        </div>
+        
+            <table class="table table-bordered table-condensed table-violet">
+                <thead>
+                    <tr>
+                        <th class="col-sm-4 bg-violet">{{ Form::label('approval_days', 'Days') }}</th>
+                        <th class="col-sm-4 bg-violet">{{ Form::label('approval_hours', 'Hours') }}</th>
+                        <th class="col-sm-4 bg-violet">{{ Form::label('approval_minutes', 'Minutes') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ Form::text('approval_days', null, ['class' => 'form-control input-sm']) }}</td>
+                        <td>{{ Form::selectRange('approval_hours', 0, 23, null, ['class' => 'form-control input-sm']) }}</td>
+                        <td>{{ Form::selectRange('approval_minutes', 0, 59, null, ['class' => 'form-control input-sm']) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
 
         <div class="checkbox">
             <label class="control-label" for="critical">
