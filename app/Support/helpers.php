b60ea776124991578d5c5fbe0eac4d3a936c6dc8
Omar Khaled <omar.coder007@gmail.com>
2017-05-29 10:16:09 +0300

    Task Commit

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,7 +1,30 @@
 <?php
+use App\Translation;
 
 function flash($message, $type = 'danger')
 {
     Session::flash('flash-message', $message);
     Session::flash('flash-type', $type);
+}
+
+function t($word, $language = '')
+{
+    $language = $language ?: app()->getLocale();
+
+    $word_exist = Translation::where('word', $word)
+        ->where('language_id', $language)->first();
+
+    if ($word_exist) {
+        if ($word_exist->translation != '') {
+            return $word_exist->translation;
+        }
+        return $word_exist->word;
+    }
+
+    $newWord = new Translation();
+    $newWord->word = $word;
+    $newWord->language_id = $language;
+    $newWord->save();
+    return $newWord->word;
+
 }
