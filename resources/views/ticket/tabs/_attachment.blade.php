d1713aa1d6f615b7ba1f239a0b8d85f8d2243ebf
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-06-26 12:00:50 +0200

    Get attachments from SDP requests

--- resources/views/ticket/tabs/_attachment.blade.php
+++ resources/views/ticket/tabs/_attachment.blade.php
@@ -13,7 +13,7 @@
         <td>{{link_to($file->path, $file->display_name, ['target' => '_blank'])}}</td>
         <td>{{$file->uploaded_by}}</td>
         <td>{{$file->created_at->format('d/m/Y H:i')}}</td>
-        <th><a href="{{url($file->path)}}" class="btn btn-xs btn-info"><i class="fa fa-download"></i></a></th>
+        <th><a href="{{url($file->path)}}" target="_blank" class="btn btn-sm btn-info"><i class="fa fa-download"></i> Download</a></th>
     </tr>
     @endforeach
 </table>
