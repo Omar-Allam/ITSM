6b856b593f323c182a5dcaa5357f34dd1426fe93
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-07-02 12:10:37 +0200

    Fix subcategory and item sync

--- app/Console/Commands/SyncServiceDeskPlus.php
+++ app/Console/Commands/SyncServiceDeskPlus.php
@@ -65,8 +65,8 @@ class SyncServiceDeskPlus extends Command
             $createdby = User::where('name', $request['createdby'])->first();
 
             $category = Category::where('name', $request['category'])->first();
-            $subcategory = Subcategory::where('name', $request['subcategory'])->first();
-            $item = Item::where('name', $request['item'])->first();
+            $subcategory = Subcategory::where('name', $request['subcategory'] ?? '')->first();
+            $item = Item::where('name', $request['item'] ?? '')->first();
 
             $query = Ticket::where('sdp_id', $request['workorderid']);
             if (!$query->exists()) {
