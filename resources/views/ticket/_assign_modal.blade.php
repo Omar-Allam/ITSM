daecd58bf8eb61ec815c4779f25f4f6958954ea1
Omar Khaled <omar.coder007@gmail.com>
2017-07-02 15:30:33 +0300

    modify reassign modal

--- resources/views/ticket/_assign_modal.blade.php
+++ resources/views/ticket/_assign_modal.blade.php
@@ -1,5 +1,5 @@
 {{Form::model($ticket, ['route' => ['ticket.reassign', $ticket], 'class' => 'modal fade', 'id' => 'AssignForm'])}}
-<div class="modal-dialog">
+<div class="modal-dialog modal-lg">
     <div class="modal-content">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
@@ -19,37 +19,39 @@
                 {{Form::select('technician_id', App\User::technicians()->selection('Select Technician'), null, ['class' => 'form-control'])}}
                 {!! $errors->first('technician_id', '<div class="help-block">:message</div>') !!}
             </div>
+            <div class="row">
+                <div class="form-group col-md-4  {{$errors->has('category_id')? 'has-error' : ''}}">
+                    {{ Form::label('category_id', t('Category'), ['class' => 'control-label']) }}
+                    {{ Form::select('category_id', App\Category::selection('Select Category'),null, ['class' => 'form-control',  'v-model' => 'category']) }}
+                    @if ($errors->has('category_id'))
+                        <div class="error-message">{{$errors->first('category_id')}}</div>
+                    @endif
+                </div>
 
-            <div class="form-group form-group-sm {{$errors->has('category_id')? 'has-error' : ''}}">
-                {{ Form::label('category_id', t('Category'), ['class' => 'control-label']) }}
-                {{ Form::select('category_id', App\Category::selection('Select Category'), null, ['class' => 'form-control', 'v-model' => 'category']) }}
-                @if ($errors->has('category_id'))
-                    <div class="error-message">{{$errors->first('category_id')}}</div>
-                @endif
+                <div class="form-group col-md-4  {{$errors->has('subcategory')? 'has-error' : ''}}">
+                    {{ Form::label('subcategory_id', t('Subcategory'), ['class' => 'control-label']) }}
+
+                    <select class="form-control" name="subcategory_id" id="subcategory_id" v-model="subcategory" >
+                        <option value="">Select Subcategory</option>
+                        <option v-for="(name, id) in subcategories" :value="id"> @{{name}}</option>
+                    </select>
+                    @if ($errors->has('subcategory_id'))
+                        <div class="error-message">{{$errors->first('subcategory_id')}}</div>
+                    @endif
+                </div>
+
+                <div class="form-group col-md-4  {{$errors->has('item_id')? 'has-error' : ''}}">
+                    {{ Form::label('item_id', t('Item'), ['class' => 'control-label']) }}
+                    <select class="form-control" name="item_id" id="item_id" v-model="item">
+                        <option value="">Select Item</option>
+                        <option v-for="(name, id) in items" :value="id" v-text="name"></option>
+                    </select>
+                    @if ($errors->has('item_id'))
+                        <div class="error-message">{{$errors->first('item_id')}}</div>
+                    @endif
+                </div>
             </div>
 
-            <div class="form-group form-group-sm {{$errors->has('subcategory')? 'has-error' : ''}}">
-                {{ Form::label('subcategory_id', t('Subcategory'), ['class' => 'control-label']) }}
-
-                <select class="form-control" name="subcategory_id" id="subcategory_id" v-model="subcategory">
-                    <option value="">Select Subcategory</option>
-                    <option v-for="(name, id) in subcategories" :value="id">@{{name}}</option>
-                </select>
-                @if ($errors->has('subcategory_id'))
-                    <div class="error-message">{{$errors->first('subcategory_id')}}</div>
-                @endif
-            </div>
-
-            <div class="form-group form-group-sm {{$errors->has('item_id')? 'has-error' : ''}}">
-                {{ Form::label('item_id', t('Item'), ['class' => 'control-label']) }}
-                <select class="form-control" name="item_id" id="item_id" v-model="item">
-                    <option value="">Select Item</option>
-                    <option v-for="(name, id) in items" :value="id" v-text="name"></option>
-                </select>
-                @if ($errors->has('item_id'))
-                    <div class="error-message">{{$errors->first('item_id')}}</div>
-                @endif
-            </div>
 
             <div id="CustomFields">
                 @include('custom-fields.render', [
@@ -61,18 +63,18 @@
 
         </div>
 
-    <div class="modal-footer">
-        <button type="submit" class="btn btn-primary"><i class="fa fa-mail-forward"></i> {{t('Assign')}}</button>
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-primary"><i class="fa fa-mail-forward"></i> {{t('Assign')}}</button>
+        </div>
     </div>
-</div>
 </div>
 {{Form::close()}}
 
 @section('javascript')
     <script>
-        var category = '{{Form::getValueAttribute('category_id')}}';
-        var subcategory = '{{Form::getValueAttribute('subcategory_id')}}';
-        var item = '{{Form::getValueAttribute('item_id')}}';
+        var category = '{{Form::getValueAttribute('category_id') ?? $ticket->category_id}}';
+        var subcategory = '{{Form::getValueAttribute('subcategory_id') ?? $ticket->subcategory_id}}';
+        var item = '{{Form::getValueAttribute('item_id') ?? $ticket->item_id}}';
     </script>
     <script src="{{asset('/js/ticket-form.js')}}"></script>
     <script src="{{asset('/js/tinymce/tinymce.min.js')}}"></script>
