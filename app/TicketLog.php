7e253e6adc4fe7acdbef19b8ee4ba35680e431fa
Hazem Mohamed <hazem.mohamed@alkifah.com>
2017-07-02 12:17:36 +0200

    Fix subcategory and item sync

--- app/TicketLog.php
+++ app/TicketLog.php
@@ -72,7 +72,7 @@ class TicketLog extends KModel
 
     public static function makeLog(Ticket $ticket, $type, $user_id = null)
     {
-        $user_id = $user_id ?: \Auth::user()->id;
+        $user_id = $user_id ?: \Auth::user()->id ?? 0;
         return $ticket->logs()->create([
             'user_id' => $user_id,
             'type' => $type,
