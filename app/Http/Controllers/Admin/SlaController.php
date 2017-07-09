da4213b6fe1baf1c5349464d66c9d030946c0024
Omar Khaled <omar.coder007@gmail.com>
2017-07-09 13:46:51 +0300

    uncomment sla update

--- app/Http/Controllers/Admin/SlaController.php
+++ app/Http/Controllers/Admin/SlaController.php
@@ -48,24 +48,24 @@ class SlaController extends Controller
 
     public function update(Sla $sla, SlaRequest $request)
     {
-        for ($i = 0; $i < count($request->enableLeveL); $i++) {
+//        for ($i = 0; $i < count($request->enableLeveL); $i++) {
+//
+//            if (isset($request->enableLeveL[$i]) && $request->enableLeveL[$i]) {
+//                $this->validate($request, ['level-' . $i => 'required','days-' . $i => 'required|min:0|integer']);
+//                $escalate_exist = EscalationLevel::where('sla_id', $sla->id)->where('level', $i + 1)->first();
+//                if (!$escalate_exist && isset($request->level[$i])) {
+//                    EscalationLevel::create([
+//                        'user_id' => User::where('name', $request->level[$i])->first()->id
+//                        , 'sla_id' => $sla->id, 'level' => '1', 'days' => $request->level_days[$i],
+//                        'hours' => $request->level_hours[$i], 'minutes' => $request->level_minutes[$i]
+//                        , 'assign' => $request->has('assign' . ($i + 1)) ? 1 : 0, 'when_escalate' => $request->option . ($i + 1)
+//                    ]);
+//                }
+//            }
+//        }
 
-            if (isset($request->enableLeveL[$i]) && $request->enableLeveL[$i]) {
-                $this->validate($request, ['level-' . $i => 'required','days-' . $i => 'required|min:0|integer']);
-                $escalate_exist = EscalationLevel::where('sla_id', $sla->id)->where('level', $i + 1)->first();
-                if (!$escalate_exist && isset($request->level[$i])) {
-                    EscalationLevel::create([
-                        'user_id' => User::where('name', $request->level[$i])->first()->id
-                        , 'sla_id' => $sla->id, 'level' => '1', 'days' => $request->level_days[$i],
-                        'hours' => $request->level_hours[$i], 'minutes' => $request->level_minutes[$i]
-                        , 'assign' => $request->has('assign' . ($i + 1)) ? 1 : 0, 'when_escalate' => $request->option . ($i + 1)
-                    ]);
-                }
-            }
-        }
-
-//        $sla->update($request->all());
-//        $sla->updateCriteria($request);
+        $sla->update($request->all());
+        $sla->updateCriteria($request);
         flash('SLA has been saved', 'success');
 
         return \Redirect::route('admin.sla.index');
