0cc877de0b7304d853ed458184415b45f7eff775
Omar Khaled <omar.coder007@gmail.com>
2017-07-18 16:02:02 +0300

    display count of requests for each scope

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -6,7 +6,7 @@
     {{ Form::open(['route' => 'ticket.scope', 'class' => 'form-inline ticket-scope heading-actions flex']) }}
     <div class="btn-group">
         <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">
-            {{t($scopes[$scope])}} ( {{\App\Ticket::scopedView($scope)->count()}} ) <span class="caret"></span>
+            {{t($scopes[$scope])}} &nbsp; <span class="badge" style="background-color: #0079b4">{{\App\Ticket::scopedView($scope)->count()}}</span> &nbsp; <span class="caret"></span>
         </button>
 
         <ul class="dropdown-menu">
