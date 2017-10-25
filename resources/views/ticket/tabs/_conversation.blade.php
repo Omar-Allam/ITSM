a51993dd26269922d9d4271c54d125bdd033cd8e
Omar Khaled <omar.coder007@gmail.com>
2017-10-25 14:20:56 +0300

    group replies and approvals in div

--- resources/views/ticket/tabs/_conversation.blade.php
+++ resources/views/ticket/tabs/_conversation.blade.php
@@ -1,45 +1,51 @@
 @if ($ticket->replies->count() || $ticket->approvals->count())
     <section class="replies">
+
         <div class="form-group clearfix">
             <a href="#ReplyForm" class="btn btn-primary pull-right"><i class="fa fa-commenting"></i> {{t('Add reply')}}
             </a>
         </div>
 
-        @foreach($ticket->replies()->latest()->get() as $reply)
-            <div class="panel panel-sm panel-{{$reply->class}}">
-                <div class="panel-heading">
-                    <h5 class="panel-title"><a href="#reply{{$reply->id}}" data-toggle="collapse">{{t('By')}}
-                            : {{$reply->user->name}} On {{$reply->created_at->format('d/m/Y H:i A')}}</a></h5>
-                </div>
-                <div class="panel-body collapse" id="reply{{$reply->id}}">
-                    <div class="reply">
-                        {!! tidy_repair_string($reply->content, [], 'utf8') !!}
-                    </div>
-                    <br>
-                    <span class="label label-default">Status: {{t($reply->status->name)}}</span>
-                </div>
-            </div>
-        @endforeach
-
-        @can('show_approvals')
-            @foreach($ticket->approvals as $approval)
-                <div class="panel panel-sm panel-warning">
+        <div class="conversation-replies">
+            @foreach($ticket->replies()->latest()->get() as $reply)
+                <div class="panel panel-sm panel-{{$reply->class}}">
                     <div class="panel-heading">
-                        <h5 class="panel-title"><a href="#approval{{$approval->id}}"
-                                                   data-toggle="collapse">{{t('Approval Submitted By')}}
-                                : {{$approval->created_by->name}}
-                                On {{$approval->created_at->format('d/m/Y H:i A')}}</a>
-                        </h5>
+                        <h5 class="panel-title"><a href="#reply{{$reply->id}}" data-toggle="collapse">{{t('By')}}
+                                : {{$reply->user->name}} On {{$reply->created_at->format('d/m/Y H:i A')}}</a></h5>
                     </div>
-                    <div class="panel-body collapse" id="approval{{$approval->id}}">
+                    <div class="panel-body collapse" id="reply{{$reply->id}}">
                         <div class="reply">
-                            {!! tidy_repair_string($approval->content, [], 'utf8') !!}
+                            {!! tidy_repair_string($reply->content, [], 'utf8') !!}
                         </div>
                         <br>
+                        <span class="label label-default">Status: {{t($reply->status->name)}}</span>
                     </div>
                 </div>
             @endforeach
+        </div>
+
+        @can('show_approvals')
+            <div class="conversation-approvals">
+                @foreach($ticket->approvals as $approval)
+                    <div class="panel panel-sm panel-warning">
+                        <div class="panel-heading">
+                            <h5 class="panel-title"><a href="#approval{{$approval->id}}"
+                                                       data-toggle="collapse">{{t('Approval Submitted By')}}
+                                    : {{$approval->created_by->name}}
+                                    On {{$approval->created_at->format('d/m/Y H:i A')}}</a>
+                            </h5>
+                        </div>
+                        <div class="panel-body collapse" id="approval{{$approval->id}}">
+                            <div class="reply">
+                                {!! tidy_repair_string($approval->content, [], 'utf8') !!}
+                            </div>
+                            <br>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         @endcan
+
     </section>
 @endif
 
@@ -73,7 +79,6 @@
                 <thead>
                 <tr>
                     <th>Attachments</th>
-
                 </tr>
                 </thead>
                 <tbody>
@@ -86,7 +91,6 @@
             </table>
         </div>
     </div>
-
 
     <div class="form-group">
         <button type="submit" class="btn btn-primary"><i class="fa fa-send"></i> {{t('Send')}}</button>
