526d1b1012409ea5b631b496971311c2a74e1f3f
Omar Khaled <omar.coder007@gmail.com>
2017-10-30 11:48:36 +0300

    stop change technician when change category

--- resources/views/ticket/_assign_modal.blade.php
+++ resources/views/ticket/_assign_modal.blade.php
@@ -12,15 +12,15 @@
                 <div class="col-md-6">
                     <div class="form-group {{$errors->first('group_id', 'has-error')}}">
                         {{Form::label('group_id', t('Group'), ['class' => 'control-label'])}}
-                        {{Form::select('group_id', App\Group::selection('Select Group'), null, ['class' => 'form-control','v-model'=>'group'])}}
+                        {{Form::select('group_id', App\Group::selection('Select Group'),null, ['class' => 'form-control','v-model'=>'group'])}}
                         {!! $errors->first('group_id', '<div class="help-block">:message</div>') !!}
                     </div>
 
                     <div class="form-group ">
                         {{ Form::label('technician_id', t('Technician'), ['class' => 'control-label']) }}
-                        <select  class="form-control" name="technician_id" id="technician_id">
+                        <select  class="form-control" name="technician_id" id="technician_id" v-model="technician_id">
                             <option value="">Select Technician</option>
-                            <option v-for="tech in technicians" :value="tech.id" :selected="tech.id == {{$ticket->technician_id ?? 0}}"> @{{tech.name}}</option>
+                            <option v-for="tech in technicians" :value="tech.id"> @{{tech.name}}</option>
                         </select>
                         @if ($errors->has('technician_id'))
                             <div class="error-message">{{$errors->first('technician_id')}}</div>
@@ -41,7 +41,7 @@
 
                         <select class="form-control" name="subcategory_id" id="subcategory_id" v-model="subcategory">
                             <option value="">Select Subcategory</option>
-                            <option v-for="subcat in subcategories" :value="subcat.id"> @{{subcat.name}}</option>
+                            <option v-for="subcat in subcategories" :value="subcat.id" v-text="subcat.name"></option>
                         </select>
 
                         @if ($errors->has('subcategory_id'))
@@ -79,13 +79,13 @@
     </div>
 </div>
 {{Form::close()}}
-
 @section('javascript')
     <script>
         var category = '{{Form::getValueAttribute('category_id') ?? $ticket->category_id}}';
         var subcategory = '{{Form::getValueAttribute('subcategory_id') ?? $ticket->subcategory_id}}';
         var item = '{{Form::getValueAttribute('item_id') ?? $ticket->item_id}}';
         var group = '{{Form::getValueAttribute('group_id') ?? $ticket->group_id}}'
+        var technician_id = '{{Form::getValueAttribute('technician_id') ?? $ticket->technician_id}}'
     </script>
     <script src="{{asset('/js/ticket-form.js')}}"></script>
     <script src="{{asset('/js/tinymce/tinymce.min.js')}}"></script>
